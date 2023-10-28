71006cd8772fd36d33766f472b133659fba2e5e1
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2023-10-29 02:21:11 +0700

    fix skeleton

--- src/presentations/components/posts/SimplePost/index.tsx
+++ src/presentations/components/posts/SimplePost/index.tsx
@@ -4,12 +4,30 @@ import { Container, SectionTitle, SimpleCard } from '@components';
 import { useLatestPosts } from '@hooks';
 import { formatDate, createAuthorDataFromPost } from '@utils';
 
+const LOADING_ITEMS = [1, 2, 3, 4];
+
 function Component() {
   const posts = useLatestPosts();
 
-  const renderContent = () => {
+  const renderItems = () => {
     if (posts.isLoading) {
-      return <SimpleCard loading={posts.isLoading} />;
+      return (
+        <>
+          {LOADING_ITEMS.map((i, idx) => (
+            <div
+              key={i}
+              className={clsx(
+                'mb-6 inline-block w-full border-b border-slate-300 px-6 pb-6 sm:mb-0 sm:border-none sm:px-0',
+                idx === LOADING_ITEMS.length - 1
+                  ? '!mb-0 !border-b-0 sm:!pb-0'
+                  : ''
+              )}
+            >
+              <SimpleCard loading={posts.isLoading} />
+            </div>
+          ))}
+        </>
+      );
     }
 
     return (
@@ -36,6 +54,10 @@ function Component() {
         ))}
       </div>
     );
+  };
+
+  const renderContent = () => {
+    return <div className="block sm:pb-8">{renderItems()}</div>;
   };
 
   return (
